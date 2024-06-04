64912e10858ec4a440642bd26e1f5396e720e0b7
Aleksandar Ristic <storiaca@gmail.com>
2024-06-04 15:39:00 +0200

    Create server

---
+++ express-crash/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
