1c5b12852ac6b87936b58e89a8949ac8f16d4bd6
Aleksandar Ristic <storiaca@gmail.com>
2025-09-17 13:31:30 +0200

    Create node routes in separate folder

--- chatify-app/backend/src/server.js
+++ chatify-app/backend/src/server.js
@@ -1,22 +1,16 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+app.use("/api/auth", authRoutes);
 
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login endpoint");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout endpoint");
-});
-
-app.listen(4000, () => console.log("Server is running on port 4000"));
-
-// 12:27
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
