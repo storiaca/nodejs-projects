6967c47cb9ea86901e2c7b13966a0e51fedb42df
Aleksandar Ristic <storiaca@gmail.com>
2025-09-19 15:27:27 +0200

    Create routes

--- chatify-app/backend/src/server.js
+++ chatify-app/backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -12,5 +13,6 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
