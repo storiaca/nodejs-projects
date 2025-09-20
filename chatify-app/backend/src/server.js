57f99209185c074870c6fe59002c05ad4f82fb3c
Aleksandar Ristic <storiaca@gmail.com>
2025-09-20 16:55:58 +0200

    Ready for deplyment

--- chatify-app/backend/src/server.js
+++ chatify-app/backend/src/server.js
@@ -1,12 +1,15 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,5 +17,13 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
