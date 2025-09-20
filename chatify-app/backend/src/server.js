ca236619a1af96fafc37332226c8f1aa683bc743
Aleksandar Ristic <storiaca@gmail.com>
2025-09-20 17:23:36 +0200

    Mongo db connection

--- chatify-app/backend/src/server.js
+++ chatify-app/backend/src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -26,4 +28,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  connectDB();
+});
