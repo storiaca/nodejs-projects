331cf324913ff9dbb54e071d0844ce1c67a8672b
Aleksandar Ristic <storiaca@gmail.com>
2025-09-27 19:01:17 +0200

    Create user via postman

--- tom-node-rest/src/controller/user.controller.ts
+++ tom-node-rest/src/controller/user.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import { omit } from "loadsh";
 import logger from "../utils/logger";
 import { createUser } from "../service/user.service";
+import { CreateUserInput } from "../schema/user.schema";
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
   try {
     const user = await createUser(req.body);
-    return user;
+    return res.send(omit(user.toJSON(), "password"));
   } catch (e: unknown) {
     logger.error(e);
     const errorMessage = e instanceof Error ? e.message : "Unknown error";
