b6b35e3e71816cf9b27cd226f954c616768819aa
Aleksandar Ristic <storiaca@gmail.com>
2025-09-28 12:41:08 +0200

    Create user service

---
+++ tom-node-rest/src/controller/session.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+export async function createUserSessionhanlder(req: Request, res: Response) {
+  // Validate the user's password
+  // Create a session
+  // Create an access token
+  // Create a refresh token
+  // return access & refresh tokens
+}
--- tom-node-rest/src/service/user.service.ts
+++ tom-node-rest/src/service/user.service.ts
@@ -1,4 +1,5 @@
 import { DocumentDefinition } from "mongoose";
+import { omit } from "lodash";
 import UserModel, { UserDocument } from "../models/user.model";
 
 export async function createUser(
@@ -7,9 +8,28 @@ export async function createUser(
   >
 ) {
   try {
-    return await UserModel.create(input);
+    const user = await UserModel.create(input);
+    return omit(user.toJSON(), "password");
   } catch (e: unknown) {
     const errorMessage = e instanceof Error ? e.message : "Unknown error";
     throw new Error(errorMessage);
   }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) return false;
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), "password");
 }
