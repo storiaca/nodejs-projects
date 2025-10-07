2d64d7f718580e5c9202fa0b3466dc7cefef5d7a
Aleksandar Ristic <storiaca@gmail.com>
2025-10-07 17:09:40 +0200

    Ts node start

--- tom-node-rest/src/middleware/deserializeUser.ts
+++ tom-node-rest/src/middleware/deserializeUser.ts
@@ -1,2 +1,3 @@
 import { Request, Response, NextFunction } from "express";
 const deserializeUser = (req: Request, res: Response, next: NextFunction) => {};
+// 1:17:47
---
+++ ts-node-start/README.md
@@ -0,0 +1,5 @@
+# Setting Up Node.js + Express + TypeScript the Right Way
+
+## Video
+
+<https://www.youtube.com/watch?v=oODlPLfnTIk&t=156s>
