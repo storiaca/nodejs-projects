2e7114dcbb300072b4cb4371348410d3c35d793b
Aleksandar Ristic <storiaca@gmail.com>
2025-10-09 17:49:37 +0200

    Ts node setup

---
+++ ts-node-start/src/index.ts
@@ -0,0 +1,5 @@
+import app from "./server";
+
+app.listen(5566, () => {
+  console.log("Server is running on 5566 port");
+});
---
+++ ts-node-start/src/server.ts
@@ -0,0 +1,10 @@
+// import express, { Request, Response } from "express";
+import * as express from "express";
+
+const app = express();
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Server is running fine");
+// });
+
+export default app;
