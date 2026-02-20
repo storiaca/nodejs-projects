19a9e14eac27370334f78f1a2a8216af9b7de671
Aleksandar Ristic <storiaca@gmail.com>
2026-02-20 10:59:52 +0100

    Create post requst

--- back-node-full/chapter_2/server.js
+++ back-node-full/chapter_2/server.js
@@ -5,6 +5,9 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+// Middleware
+app.use(express.json())
+
 // ENDPOINT - HTTP Verbs (method) && Routes
 // The method informs the nature of request and the route is a further subdirectory (basically we direct the request to the body of code to respond appropriately, and these loactions or  routes are called endpoints)
 
@@ -17,13 +20,19 @@ app.get("/", (req, res) => {
   // this is endpoint number 1 - /
   // console.log("Yay I hit an endpoint", req.method);
   // res.sendStatus(201)
-  res.send('<h1>Homepage</h1>')
+  res.send(`
+    <body style="background:pink;color:blue;">
+      <h1>Data:</h1>
+      <p>${JSON.stringify(data)}</p>
+    </body>
+  `)
 });
 
 app.get('/dashboard', (req, res) => {
   // console.log('Ohh now I hit the /dahsboard endpoint');
-  res.send('<h1>Dashboard</h1>')  
+  res.send("<h1>Homepage</h1>")  
 })
+
 
 // Type 2 - API endpoints (non visual)
 
@@ -31,6 +40,16 @@ app.get('/dashboard', (req, res) => {
 app.get('/api/data', (req, res) => {
   console.log('This one was for data');
   res.send(data)
+})
+
+/**
+ * POST Route
+ */
+app.post("/api/data", (req, res) => {
+  const newEntry = req.body
+  console.log(newEntry);
+  
+  res.sendStatus(201)
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
