e35b19df864b526414ab4dcb86db835f59187de0
Aleksandar Ristic <storiaca@gmail.com>
2026-02-25 12:50:20 +0100

    Configure server

--- back-node-full/chapter_3/src/server.js
+++ back-node-full/chapter_3/src/server.js
@@ -1,1 +1,9 @@
-console.log("server");
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`)
+})
