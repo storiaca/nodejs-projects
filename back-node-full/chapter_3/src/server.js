7bd2f268d9a08e3b495562f85b547aead0a54f46
Aleksandar Ristic <storiaca@gmail.com>
2026-02-26 16:47:30 +0100

    Show html file from server

--- back-node-full/chapter_3/src/server.js
+++ back-node-full/chapter_3/src/server.js
@@ -1,9 +1,25 @@
-import express from 'express'
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets /
+// file. Any request for the css files will be resolved to the public directory
+app.use(express.static(path.join(__dirname, "../public")))
+
+// Serving up the HTML file from the /public directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+});
 
 app.listen(PORT, () => {
-  console.log(`Server has started on port: ${PORT}`)
-})
+  console.log(`Server has started on port: ${PORT}`);
+});
