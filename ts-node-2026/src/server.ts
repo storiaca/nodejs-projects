04a0d52adc30dd9bfd20960545bb2e4c144a91fe
Aleksandar Ristic <storiaca@gmail.com>
2026-03-03 13:10:00 +0100

    Create statci folder public for files

--- ts-node-2026/src/server.ts
+++ ts-node-2026/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path"
 
 const app = express();
 
 // Set template engine
 app.set("view engine", "ejs");
+// Middleware for static files
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 app.get("/", (req, res) => {
   // res.status(200).send("Hello world again and again!");
