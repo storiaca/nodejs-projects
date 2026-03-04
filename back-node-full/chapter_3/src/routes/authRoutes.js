dccc8f97de0fd3b0417c0ef353299a8dbc75538d
Aleksandar Ristic <storiaca@gmail.com>
2026-03-04 08:58:49 +0100

    Create code for registration

--- back-node-full/chapter_3/src/routes/authRoutes.js
+++ back-node-full/chapter_3/src/routes/authRoutes.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import db from "../db.js"
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import db from "../db.js";
 
-const router = express.Router()
+const router = express.Router();
 
+// Register a new user endpoint /auth/register
 router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password)
+  res.sendStatus(201)
+});
 
-})
+router.post("/login", (req, res) => {});
 
-router.post("/login", (req, res) => {
-
-})
-
-export default router
+export default router;
