01d5d14090d6f8a845cccb3414a6723a1503df75
Aleksandar Ristic <storiaca@gmail.com>
2026-03-18 11:05:11 +0100

    Auth middleware

--- back-node-full/chapter_3/src/middleware/authMiddleware.js
+++ back-node-full/chapter_3/src/middleware/authMiddleware.js
@@ -1,1 +1,22 @@
-console.log("Middleware");
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+function authMiddleware(req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    req.userId = decoded.id
+
+    next()
+  });
+}
+
+export default authMiddleware
--- back-node-full/chapter_3/src/routes/todoRoutes.js
+++ back-node-full/chapter_3/src/routes/todoRoutes.js
@@ -7,7 +7,9 @@ const router = express.Router()
 
 // Get all todos for logged-in user
 router.get("/", (req, res) => {
-
+  const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+  const todos = getTodos.all(req.userId)
+  res.json(todos)
 })
 // Create a new todo
 router.post("/", (req, res) => {
