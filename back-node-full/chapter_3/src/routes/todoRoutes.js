1ba315a2bc0755eaebaa746b93ceff7c21c75225
Aleksandar Ristic <storiaca@gmail.com>
2026-03-19 11:36:21 +0100

    Create todo route

--- back-node-full/chapter_3/src/routes/todoRoutes.js
+++ back-node-full/chapter_3/src/routes/todoRoutes.js
@@ -1,27 +1,32 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import db from "../db.js"
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import db from "../db.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // Get all todos for logged-in user
 router.get("/", (req, res) => {
-  const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
-  const todos = getTodos.all(req.userId)
-  res.json(todos)
-})
+  const getTodos = db.prepare("SELECT * FROM todos WHERE user_id = ?");
+  const todos = getTodos.all(req.userId);
+  res.json(todos);
+});
+
 // Create a new todo
 router.post("/", (req, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(
+    `INSERT INTO todos (user_id, task) VALUES (?,?)`,
+  );
+  const result = insertTodo.run(req.userId, task);
 
-})
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
+
 // Update a todo
-router.put("/:id", (req, res) => {
+router.put("/:id", (req, res) => {});
 
-})
 // Delete a todo
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-})
-
-export default router
+export default router;
