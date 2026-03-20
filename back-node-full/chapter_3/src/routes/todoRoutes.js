322010c96efd6916c76e0c707846d468907c0d0f
Aleksandar Ristic <storiaca@gmail.com>
2026-03-20 10:47:35 +0100

    Delete todo

--- back-node-full/chapter_3/src/routes/todoRoutes.js
+++ back-node-full/chapter_3/src/routes/todoRoutes.js
@@ -36,6 +36,14 @@ router.put("/:id", (req, res) => {
 });
 
 // Delete a todo
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const userId = req.userId
+  
+  const deleteTodo = db.prepare(
+    "DELETE FROM todos WHERE id = ? AND user_id = ?",
+  );
+  deleteTodo.run(id, userId);
+});
 
 export default router;
