9965ea487f05fbe274bf2aa0f94bf99e0859f1ae
Aleksandar Ristic <storiaca@gmail.com>
2026-03-22 20:14:14 +0100

    First route

--- back-exp-pg-prism-2026/src/server.js
+++ back-exp-pg-prism-2026/src/server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = 5555
 
+app.get("/hello", (req, res) => {
+  res.json({ message: 'Hello World!'})
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`)
 })
