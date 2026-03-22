36fc95f64562e75ba7c98593f8cca997db41a2fa
Aleksandar Ristic <storiaca@gmail.com>
2026-03-22 20:27:01 +0100

    Movie routes

--- back-exp-pg-prism-2026/src/server.js
+++ back-exp-pg-prism-2026/src/server.js
@@ -1,13 +1,26 @@
-const express = require("express")
+import express from "express"
+
+// Import Routes
+import movieRoutes from "./routes/movieRoutes.js"
 
 const app = express()
 
+// API Routes
+app.use("/movies", movieRoutes)
+
 const PORT = 5555
 
-app.get("/hello", (req, res) => {
-  res.json({ message: 'Hello World!'})
-})
+// app.get("/hello", (req, res) => {
+//   res.json({ message: 'Hello World!'})
+// })
 
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`)
 })
+
+// http://localhost:5555
+
+// AUTH - signin, signup
+// MOVIE - Getting all movies
+// USER - Profile
+// WATCHLIST
