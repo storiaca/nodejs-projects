9e6c78eb6aaf4097fcc79ddd33c28940926b3c17
Aleksandar Ristic <storiaca@gmail.com>
2026-03-22 20:30:01 +0100

    Adding more request types

--- back-exp-pg-prism-2026/src/routes/movieRoutes.js
+++ back-exp-pg-prism-2026/src/routes/movieRoutes.js
@@ -2,8 +2,20 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
-  res.json({ message: "hello" });
+router.get("/", (req, res) => {
+  res.json({ message: "GET" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "POST" });
+});
+
+router.put("/", (req, res) => {
+  res.json({ message: "PUT" });
+});
+
+router.delete("/", (req, res) => {
+  res.json({ message: "DELETE" });
 });
 
 export default router
