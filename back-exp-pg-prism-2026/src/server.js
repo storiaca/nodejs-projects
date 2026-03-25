dc0d7c1f8b9351f471a1cf8dfcf1737b469b2c60
Aleksandar Ristic <storiaca@gmail.com>
2026-03-25 11:48:14 +0100

    Define middleware for body request

--- back-exp-pg-prism-2026/src/server.js
+++ back-exp-pg-prism-2026/src/server.js
@@ -1,54 +1,57 @@
-import express from "express"
+import express from "express";
 
-import { connectDB, disconnectDB } from "./config/db.js"
+import { connectDB, disconnectDB } from "./config/db.js";
 
 // Import Routes
-import movieRoutes from "./routes/movieRoutes.js"
-import authRoutes from "./routes/authRoutes.js"
+import movieRoutes from "./routes/movieRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
+
+// Body parsing middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API Routes
-app.use("/movies", movieRoutes)
-app.use("/auth", authRoutes)
+app.use("/movies", movieRoutes);
+app.use("/auth", authRoutes);
 
-const PORT = 5555
+const PORT = 5555;
 
 // app.get("/hello", (req, res) => {
 //   res.json({ message: 'Hello World!'})
 // })
 
 app.listen(PORT, () => {
-  console.log(`Server running on Port: ${PORT}`)
-})
+  console.log(`Server running on Port: ${PORT}`);
+});
 
 // Handle unhandled promise rejections (e.g. database connection errors)
 process.on("unhandledRejection", (err) => {
-  console.error("Unhandled Rejection:", err)
+  console.error("Unhandled Rejection:", err);
   server.close(async () => {
-    await disconnectDB()
-    process.exit(1)
-  })
-})
+    await disconnectDB();
+    process.exit(1);
+  });
+});
 
 // Handle uncaught exceptions
-process.on("uncaughtException", async(err) => {
-  console.error("Uncaught Exception:", err)
-  await disconnectDB()
-  process.exit(1)
-})
+process.on("uncaughtException", async (err) => {
+  console.error("Uncaught Exception:", err);
+  await disconnectDB();
+  process.exit(1);
+});
 
 // Graceful shutdown
-process.on("SIGTERM", async() => {
-  console.log("SIGTERM rev=ceived, shutting down gracefully")
-  sever.close(async() => {
-    await disconnectDB()
-    process.exit(1)
-  })
-})
-
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM rev=ceived, shutting down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
 
 // http://localhost:5555
 
