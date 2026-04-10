6e90b43fb406b9b1cc31e095f68eb3cd590717be
Aleksandar Ristic <storiaca@gmail.com>
2026-04-10 14:50:19 +0200

    Error message start

--- 2026-rest-ts-db/src/controllers/taskController.ts
+++ 2026-rest-ts-db/src/controllers/taskController.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const listTasks = (req: Request, res: Response) => {
   res.status(200).json([]);
 };
 
-export const getTask = (req: Request, res: Response) => {
+export const getTask = async (
+  req: Request,
+  res: Response,
+) => {
+ // throw new Error("Oops");
   res.status(200).json({ id: 1, name: "Task 1" });
 };
