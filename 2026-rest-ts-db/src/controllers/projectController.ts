e9602002859cd5b92ef67b6962c026fb3e4f8306
Aleksandar Ristic <storiaca@gmail.com>
2026-04-13 12:09:33 +0200

    Add code in controllers and fix prisma client for mysql

--- 2026-rest-ts-db/src/controllers/projectController.ts
+++ 2026-rest-ts-db/src/controllers/projectController.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from "express";
+import prisma from "../prisma-client";
+import EntityNotFoundError from "../errors/EntityNotFoundError";
 
-export const listProjects = (req: Request, res: Response) => {
-  res.status(200).json([]);
+export const listProjects = async (req: Request, res: Response) => {
+  const projects = await prisma.project.findMany();
+  res.status(200).json({ projects });
 };
 
-export const getProject = (req: Request, res: Response) => {
-  res.status(200).json({ id: 1, name: "Project 1" });
+export const getProject = async (req: Request, res: Response) => {
+  const project = await prisma.project.findUnique({
+    where: {
+      id: req.params.id as string
+    }
+  })
+
+  if(!project) {
+    throw new EntityNotFoundError({
+      message: "Project not found",
+      statusCode: 404,
+      code: "ERR_NF"
+    })
+  }
+  res.status(200).json({ project });
 };
 
-export const listProjectTasks = (req: Request, res: Response) => {
-  res.status(200).json([]);
+export const listProjectTasks = async(req: Request, res: Response) => {
+  const tasks = await prisma.task.findMany({
+    where: {
+      project_id: req.params.id as string
+    }
+  })
+  res.status(200).json({tasks});
 };
--- 2026-rest-ts-db/src/controllers/taskController.ts
+++ 2026-rest-ts-db/src/controllers/taskController.ts
@@ -1,18 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import EntityNotFoundError from "../errors/EntityNotFoundError";
+import prisma from "../prisma-client";
 
-export const listTasks = (req: Request, res: Response) => {
-  res.status(200).json([]);
+export const listTasks = async (req: Request, res: Response) => {
+  const tasks = await prisma.task.findMany();
+  res.status(200).json({ tasks });
 };
 
-export const getTask = async (
-  req: Request,
-  res: Response,
-) => {
-  // throw new EntityNotFoundError({
-  //   message: "Entity not found",
-  //   statusCode: 404,
-  //   code: "ERR_NF"
-  // });
-  res.status(200).json({ id: 1, name: "Task 1" });
+export const getTask = async (req: Request, res: Response) => {
+  
+  const task = await prisma.task.findUnique({
+    where: {
+      id: req.params.id as string
+    }
+  })
+
+  if(!task) {
+    throw new EntityNotFoundError({
+    message: "Task not found",
+    statusCode: 404,
+    code: "ERR_NF"
+  });
+  }
+  res.status(200).json({ task });
 };
