deac5e34463a6ea30bc64366979966eb849843d4
Aleksandar Ristic <storiaca@gmail.com>
2026-05-13 20:50:48 +0200

    Clean user controller

--- 2026-jira-api/src/controllers/user.controller.ts
+++ 2026-jira-api/src/controllers/user.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { createUser } from "../services/user.service.js";
-const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-]{2,}$/;
 
 export const getUsers = async (req: Request, res: Response) => {
   res.status(200).json({ message: "List all users" });
@@ -9,15 +8,6 @@ export const getUsers = async (req: Request, res: Response) => {
 export const createUserController = async (req: Request, res: Response) => {
   try {
     const { email, name } = req.body;
-
-    if (!EMAIL_REGEX.test(email)) {
-      return res.status(400).json({ error: "Invalid email format" });
-    }
-
-    if (!email) {
-      return res.status(400).json({ error: "Email is required" });
-    }
-
     const user = await createUser({ email, name });
 
     res.status(201).json(user);
--- 2026-jira-api/src/validators/user.validator.ts
+++ 2026-jira-api/src/validators/user.validator.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 const createUserSchema = z.object({
   email: z.email(),
-  name: z.string().min(2).trim(),
+  name: z.string().min(2).max(50).trim(),
 });
 
 export { createUserSchema };
